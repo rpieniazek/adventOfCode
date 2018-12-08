5ceff566a1b85ab8bbafb5940121e44bc465350a
rafalpieniazek <rkpieniazek@gmail.com>
2018-12-08 22:46:02 +0100

    day3

--- src/day3/day3task1.kt
+++ src/day3/day3task1.kt
@@ -1,50 +1,42 @@
 package day3
 
+import java.io.IOException
+import java.nio.file.Files
+import java.nio.file.Paths
+
 fun main(args: Array<String>) {
 
-    fun main(args: Array<String>) {
-        val word = "#1 @ 1,3: 4x4"
-        val s = word.replace("#\\d+ @ ".toRegex(), "")
-                .replace(" ".toRegex(), "")
-                .replace("[#@,:x]".toRegex(), ",")
-        //                System.out.println(s);
-        val splitted = s.split(",".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
-        print(splitted)
-        val tab = Array(10000) { IntArray(10000) }
-//        try {
-//            Files.lines(Paths.get("input.txt")).forEach { l ->
-//                val s = l.replace("#\\d+ @ ".toRegex(), "")
-//                        .replace(" ".toRegex(), "")
-//                        .replace("[#@,:x]".toRegex(), ",")
-//                //                System.out.println(s);
-//                val splitted = s.split(",".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
-//                val xCoord = Integer.parseInt(splitted[0])
-//                val yCoord = Integer.parseInt(splitted[1])
-//                val xCanvasSize = Integer.parseInt(splitted[2])
-//                val yCanvasSize = Integer.parseInt(splitted[3])
-//                for (i in 0 until xCanvasSize) {
-//                    for (j in 0 until yCanvasSize) {
-//                        tab[xCoord + i][yCoord + j]++
-//                    }
-//                }
-//
-//
-//            }
-//            var counter = 0
-//
-//            for (i in tab.indices) {
-//                for (j in tab.indices) {
-//
-//                    if (tab[i][j] >= 2) {
-//                        counter++
-//                    }
-//                }
-//
-//            }
-//            println(counter)
-//        } catch (e: IOException) {
-//            e.printStackTrace()
-//        }
 
+    val tab = Array(10000) { IntArray(10000) }
+    try {
+        Files.lines(Paths.get("src/day3/input.txt")).forEach { l ->
+            val s = l.replace("#\\d+ @ ".toRegex(), "")
+                    .replace(" ".toRegex(), "")
+                    .replace("[#@,:x]".toRegex(), ",")
+            //                System.out.println(s);
+            val splitted = s.split(",".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+            val xCoord = Integer.parseInt(splitted[0])
+            val yCoord = Integer.parseInt(splitted[1])
+            val xCanvasSize = Integer.parseInt(splitted[2])
+            val yCanvasSize = Integer.parseInt(splitted[3])
+            for (i in 0 until xCanvasSize) {
+                for (j in 0 until yCanvasSize) {
+                    tab[xCoord + i][yCoord + j]++
+                }
+            }
+        }
+        var counter = 0
+
+        for (i in tab.indices) {
+            for (j in tab.indices) {
+                if (tab[i][j] >= 2) {
+                    counter++
+                }
+            }
+
+        }
+        println(counter)
+    } catch (e: IOException) {
+        e.printStackTrace()
     }
 }
