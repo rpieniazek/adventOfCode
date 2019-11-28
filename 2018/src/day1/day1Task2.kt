ea742055dcad24a22c017d2fa07ba78073d13a62
rafalpieniazek <rafal.pieniazek@collibra.com>
2019-11-28 09:59:52 +0100

    moved to 2018 dir

---
+++ 2018/src/day1/day1Task2.kt
@@ -0,0 +1,29 @@
+package day1
+
+import java.io.File
+
+
+fun main(args: Array<String>) {
+    val frequences: MutableSet<Int> = HashSet<Int>();
+    val changes:MutableList<Int> = ArrayList<Int>();
+
+    File("src/day1/input.txt").useLines { lines ->
+        lines.forEach { line ->
+            changes.add(line.toInt())
+        }
+    }
+    var sum = 0;
+    do {
+        changes.forEach {change ->
+            sum += change
+            if(!frequences.add(sum)){
+                println(sum)
+                return
+            }
+        }
+    }
+    while(true)
+
+
+}
+
